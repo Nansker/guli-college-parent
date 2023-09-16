4063de5153ac0c949e6b61061daeeb14bc3bceaf
南山客 <nansker@163.com>
2023-09-17 01:29:55 +0800

    实现了邮件发送功能
    Signed-off-by: 南山客 <nansker@163.com>

---
+++ service/service-mail/src/main/java/com/nansker/mail/controller/MailController.java
@@ -0,0 +1,25 @@
+package com.nansker.mail.controller;
+
+import com.nansker.mail.service.MailService;
+import com.nansker.utils.result.ResultData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Nansker
+ * @date 2023/9/17 1:12
+ * @description TODO
+ */
+@RestController
+@RequestMapping("/mail")
+public class MailController {
+	@Autowired
+	private MailService mailService;
+
+	@RequestMapping
+	public ResultData sendSimpleMail(){
+		mailService.sendSimpleMail("2771557108@qq.com","测试标题","测试文本内容");
+		return ResultData.ok();
+	}
+}
